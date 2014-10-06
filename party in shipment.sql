4db95d0e7e6fd206c519ea2e1e9757fd00247c59
Aji Tri Santoso/ 1103120130 <ajitrisantoso@gmail.com>
2014-10-07 00:31:26 +0700

    Update party in shipment.sql
    perbaikan subtype & supertype

--- party in shipment.sql
+++ party in shipment.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 21, 2014 at 06:39 PM
+-- Generation Time: Oct 06, 2014 at 07:29 PM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.9
 
@@ -65,8 +65,41 @@ CREATE TABLE IF NOT EXISTS `gender_type` (
 --
 
 INSERT INTO `gender_type` (`gender_type_id`, `description`) VALUES
-(1, 'Pria'),
-(2, 'Wanita');
+(1, 'Male'),
+(2, 'Female'),
+(3, 'Male to Fe'),
+(4, 'Female to '),
+(5, 'Not Provid');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `informal_organization`
+--
+
+CREATE TABLE IF NOT EXISTS `informal_organization` (
+  `organization_id` int(11) NOT NULL,
+  PRIMARY KEY (`organization_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `legal_organization`
+--
+
+CREATE TABLE IF NOT EXISTS `legal_organization` (
+  `organization_id` int(11) NOT NULL,
+  `federal_tax_id_num` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`organization_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='organization subtype';
+
+--
+-- Dumping data for table `legal_organization`
+--
+
+INSERT INTO `legal_organization` (`organization_id`, `federal_tax_id_num`) VALUES
+(1, '1103120130');
 
 -- --------------------------------------------------------
 
@@ -97,78 +130,17 @@ INSERT INTO `marital_status_type` (`marital_status_type_id`, `description`) VALU
 --
 
 CREATE TABLE IF NOT EXISTS `organization` (
-  `organization_id` int(10) NOT NULL,
-  `organization_name` varchar(50) NOT NULL,
-  `organization_role` int(2) NOT NULL,
-  PRIMARY KEY (`organization_id`),
-  KEY `organization_role` (`organization_role`)
+  `party_id` int(11) NOT NULL,
+  `organization_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`party_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `organization`
 --
 
-INSERT INTO `organization` (`organization_id`, `organization_name`, `organization_role`) VALUES
-(1, 'PT. AKSARA', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `organization_role`
---
-
-CREATE TABLE IF NOT EXISTS `organization_role` (
-  `organization_role_id` int(1) NOT NULL,
-  `description` varchar(30) NOT NULL,
-  `organization_unit_id` int(2) NOT NULL,
-  PRIMARY KEY (`organization_role_id`),
-  KEY `organization_unit_id` (`organization_unit_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `organization_role`
---
-
-INSERT INTO `organization_role` (`organization_role_id`, `description`, `organization_unit_id`) VALUES
-(1, 'Partner', 1),
-(2, 'Partner', 2),
-(3, 'Partner', 3),
-(4, 'Partner', 4),
-(5, 'Supplier', 1),
-(6, 'Supplier', 2),
-(7, 'Supplier', 3),
-(8, 'Supplier', 4),
-(9, 'Household', 1),
-(10, 'Household', 2),
-(11, 'Household', 3),
-(12, 'Household', 4),
-(13, 'Personal', 4),
-(14, 'Internal Organization', 1),
-(15, 'Internal Organization', 2),
-(16, 'Internal Organization', 3),
-(17, 'Internal Organization', 4);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `organization_unit`
---
-
-CREATE TABLE IF NOT EXISTS `organization_unit` (
-  `organization_unit_id` int(2) NOT NULL,
-  `description` varchar(40) NOT NULL,
-  PRIMARY KEY (`organization_unit_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `organization_unit`
---
-
-INSERT INTO `organization_unit` (`organization_unit_id`, `description`) VALUES
-(1, 'Parent Organization'),
-(2, 'Department'),
-(3, 'Division'),
-(4, 'Other Organization U');
+INSERT INTO `organization` (`party_id`, `organization_name`) VALUES
+(1, 'Bentang Pustaka');
 
 -- --------------------------------------------------------
 
@@ -177,20 +149,16 @@ INSERT INTO `organization_unit` (`organization_unit_id`, `description`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `party` (
-  `party_id` int(10) NOT NULL,
-  `person_id` int(2) NOT NULL,
-  `organization_id` int(2) NOT NULL,
-  PRIMARY KEY (`party_id`),
-  KEY `person_id` (`person_id`,`organization_id`),
-  KEY `organization_id` (`organization_id`)
+  `party_id` int(11) NOT NULL,
+  PRIMARY KEY (`party_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `party`
 --
 
-INSERT INTO `party` (`party_id`, `person_id`, `organization_id`) VALUES
-(1, 1, 1);
+INSERT INTO `party` (`party_id`) VALUES
+(1);
 
 -- --------------------------------------------------------
 
@@ -199,7 +167,7 @@ INSERT INTO `party` (`party_id`, `person_id`, `organization_id`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `person` (
-  `person_id` int(10) NOT NULL,
+  `party_id` int(11) NOT NULL,
   `id_number` varchar(16) NOT NULL,
   `full_name` varchar(40) NOT NULL,
   `address` varchar(100) NOT NULL,
@@ -209,7 +177,7 @@ CREATE TABLE IF NOT EXISTS `person` (
   `gender_type_id` int(1) NOT NULL,
   `marital_status_type_id` int(1) NOT NULL,
   `country_id` int(2) NOT NULL,
-  PRIMARY KEY (`person_id`),
+  PRIMARY KEY (`party_id`),
   KEY `gender_type_id` (`gender_type_id`,`marital_status_type_id`),
   KEY `country_id` (`country_id`),
   KEY `country_id_2` (`country_id`),
@@ -220,36 +188,36 @@ CREATE TABLE IF NOT EXISTS `person` (
 -- Dumping data for table `person`
 --
 
-INSERT INTO `person` (`person_id`, `id_number`, `full_name`, `address`, `birth_date`, `mother_maiden_name`, `comment`, `gender_type_id`, `marital_status_type_id`, `country_id`) VALUES
-(1, '3310012209930005', 'Aji Tri Santoso', 'Jl. Kejaksaan Tinggi 128, Sukapura, Dayeuh Kolot.', '2014-09-22', 'n/a', 'admin', 1, 1, 1);
+INSERT INTO `person` (`party_id`, `id_number`, `full_name`, `address`, `birth_date`, `mother_maiden_name`, `comment`, `gender_type_id`, `marital_status_type_id`, `country_id`) VALUES
+(1, '33110323241212', 'Agatha Christie', 'Jl. Anggrek 26, Medan', '2014-10-08', 'Christie Hakim', 'loyal', 2, 1, 1);
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `informal_organization`
+--
+ALTER TABLE `informal_organization`
+  ADD CONSTRAINT `fk_org_inf` FOREIGN KEY (`organization_id`) REFERENCES `organization` (`party_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `legal_organization`
+--
+ALTER TABLE `legal_organization`
+  ADD CONSTRAINT `fk_org` FOREIGN KEY (`organization_id`) REFERENCES `organization` (`party_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `organization`
 --
 ALTER TABLE `organization`
-  ADD CONSTRAINT `fk_org_role` FOREIGN KEY (`organization_role`) REFERENCES `organization_role` (`organization_role_id`);
-
---
--- Constraints for table `organization_role`
---
-ALTER TABLE `organization_role`
-  ADD CONSTRAINT `fk_role_unit` FOREIGN KEY (`organization_unit_id`) REFERENCES `organization_unit` (`organization_unit_id`);
-
---
--- Constraints for table `party`
---
-ALTER TABLE `party`
-  ADD CONSTRAINT `fk_party_org` FOREIGN KEY (`organization_id`) REFERENCES `organization` (`organization_id`),
-  ADD CONSTRAINT `fk_party_person` FOREIGN KEY (`person_id`) REFERENCES `person` (`person_id`);
+  ADD CONSTRAINT `fk_party_org` FOREIGN KEY (`party_id`) REFERENCES `party` (`party_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `person`
 --
 ALTER TABLE `person`
+  ADD CONSTRAINT `fk_party` FOREIGN KEY (`party_id`) REFERENCES `party` (`party_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_person_country` FOREIGN KEY (`country_id`) REFERENCES `country` (`country_id`),
   ADD CONSTRAINT `fk_person_gender` FOREIGN KEY (`gender_type_id`) REFERENCES `gender_type` (`gender_type_ID`),
   ADD CONSTRAINT `fk_person_marital` FOREIGN KEY (`marital_status_type_id`) REFERENCES `marital_status_type` (`marital_status_type_id`);
